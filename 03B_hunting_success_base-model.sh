21c13bb92720588920de7eb323567263ee4fdead
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2021-03-18 15:49:30 -0400

    reduce cores

--- 03B_hunting_success_base-model.sh
+++ 03B_hunting_success_base-model.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-monti
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=60
+#SBATCH --cpus-per-task=40
 #SBATCH --mem=60000M
-#SBATCH --time=0-24:00
+#SBATCH --time=0-30:00
 #SBATCH --mail-user=fraser_franco.maxime@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 
