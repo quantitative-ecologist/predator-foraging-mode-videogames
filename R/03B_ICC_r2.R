1549fbc474ad397588996fc23c82ce2973cf7dae
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2021-05-11 11:47:37 -0400

    update ICC .R file

--- R/03B_ICC_r2.R
+++ R/03B_ICC_r2.R
@@ -1,6 +1,6 @@
 #########################################################################
 
-#                  Model fit and ICCs for the base model                #
+#                 Model fit and ICCs for the base models                #
 
 #########################################################################
 
@@ -32,7 +32,8 @@ data <- fread("./data/02_merged-data.csv",
                          stringsAsFactors = TRUE)
 
 # Load model
-load("./outputs/03B_hunting_success_base-model.rda")
+load("./outputs/03B_hunting_success_base-model1.rda")
+base_model2 <- readRDS("./outputs/03B_hunting_success_base-model2.rds")
 # =======================================================================
 # =======================================================================
 
@@ -48,16 +49,18 @@ load("./outputs/03B_hunting_success_base-model.rda")
 
 # Compute the model matrixes
 mm1 <- model_get_model_matrix(base_model)
+mm2 <- model_get_model_matrix(base_model2)
 
 # Variance components :
 # 1. Fixed effects variance
 # --------------------------
 # Compute variance in fitted values (Fixed effects variance)
 VarF1 <- var(as.vector(mm1%*%fixef(base_model)))
+VarF2 <- var(as.vector(mm1%*%fixef(base_model2)))
 
 # 2. Distribution-specific variance
 # --------------------------
-# For binomial model with OLRE
+# For a binomial model with OLRE
 VarDS1 <- pi^2/3
 
 # For beta binomial model
@@ -69,12 +72,17 @@ VarDS1 <- pi^2/3
 VarR1 <- VarCorr(base_model)$mirrors_id$sd[1]^2 + 
          VarCorr(base_model)$map_name$sd[1]^2
 
+VarR2 <- VarCorr(base_model2)$mirrors_id$sd[1]^2 + 
+         VarCorr(base_model2)$map_name$sd[1]^2
+
 VarSE1 <- VarCorr(base_model)$obs$sd[1]^2
+VarSE2 <- VarCorr(base_model2)$obs$sd[1]^2
 
 # 4. Total variance
 # --------------------------
 # binomial model with OLRE
 VarT1 <- VarF1 + VarR1 + VarSE1 + VarDS1
+VarT2 <- VarF2 + VarR2 + VarSE2 + VarDS1
 
 # beta-binomial model
 #VarT1 <- VarF1 + VarR1 + VarDS1
@@ -84,10 +92,15 @@ VarT1 <- VarF1 + VarR1 + VarSE1 + VarDS1
 # Marginal R2
 R2_M1 <- VarF1 /
          VarT1
+R2_M2 <- VarF2 /
+         VarT2
+
 
 # Conditional R2 (OLRE is excluded in the numerator to only account for random effects)
 R2_C1 <- (VarF1 + VarR1) / # Fixed effect variance + random effect variance
           VarT1                   # Total variance
+R2_C2 <- (VarF2 + VarR2) /
+          VarT2
 
 # =======================================================================
 # =======================================================================
@@ -106,18 +119,36 @@ ran_var <- data.table(posterior_samples(base_model,
                             "sd_map_name__Intercept",
                             "sd_obs__Intercept")))
 
+ran_var2 <- data.table(posterior_samples(base_model2, 
+                  pars = c("sd_mirrors_id__Intercept",
+                            "sd_map_name__Intercept",
+                            "sd_obs__Intercept")))
+
+
 # Compute variances for each random effect + VarDS
 ran_var[, c("var_id", "var_map", "var_obs") := 
           lapply(.SD, function(x) x^2),
             .SDcols = c(1:3)][, var_DS := VarDS1]
 
+ran_var2[, c("var_id", "var_map", "var_obs") := 
+          lapply(.SD, function(x) x^2),
+            .SDcols = c(1:3)][, var_DS := VarDS1]
+
+
 # Compute total variance            
 ran_var[, var_tot := rowSums(ran_var[, 4:7])]
+ran_var2[, var_tot := rowSums(ran_var2[, 4:7])]
+
 
 # Calculate ICCs
 ran_var[, c("icc_id", "icc_map", "icc_obs") := 
           lapply(.SD, function(x) x / var_tot),
             .SDcols = c(4:6)]
+
+ran_var2[, c("icc_id", "icc_map", "icc_obs") := 
+          lapply(.SD, function(x) x / var_tot),
+            .SDcols = c(4:6)]
+
 
 # Create table with mean icc and credibility interval
 icc_tab <- data.table(group = c("id", "map", "obs"),
@@ -126,6 +157,15 @@ icc_tab <- data.table(group = c("id", "map", "obs"),
                       rbind(coda::HPDinterval(as.mcmc(ran_var[,9]), 0.95),
                             coda::HPDinterval(as.mcmc(ran_var[,10]), 0.95),
                             coda::HPDinterval(as.mcmc(ran_var[,11]), 0.95)
+                            )
+                       )
+
+icc_tab2 <- data.table(group = c("id", "map", "obs"),
+                      mean = as.numeric(ran_var2[, lapply(.SD, mean),
+                                                  .SDcols = c(9:11)]),
+                      rbind(coda::HPDinterval(as.mcmc(ran_var2[,9]), 0.95),
+                            coda::HPDinterval(as.mcmc(ran_var2[,10]), 0.95),
+                            coda::HPDinterval(as.mcmc(ran_var2[,11]), 0.95)
                             )
                        )
 
@@ -140,15 +180,23 @@ icc_tab <- data.table(group = c("id", "map", "obs"),
 # 4. Save values in table
 # =======================================================================
 
-r2_tab <- as.data.table(cbind(R2_M1, R2_C1))
+r2_tab <- as.data.table(cbind(R2_M1, R2_C1, R2_M2, R2_C2))
+
 # Round values for icc table
 round_val <- function (x) {round(x, digits = 3)}
 icc_tab[, c("mean", "lower", "upper") :=
                 lapply(.SD, round_val), 
-                .SDcols = c(2:4)]
+                .SDcols = c(2:4)][[, model := "model1"]]
+
+icc_tab2[, c("mean", "lower", "upper") :=
+                lapply(.SD, round_val), 
+                .SDcols = c(2:4)][, model := "model2"]
+
+# Bind both tables
+icc_table <- rbind(icc_tab, icc_tab2)
 
 capture.output(r2_tab, file = "./outputs/03B_r2-table.txt")
-capture.output(icc_tab, file = "./outputs/03B_icc-table.txt")
+capture.output(icc_table, file = "./outputs/03B_icc-table.txt")
 
 # =======================================================================
 # =======================================================================
