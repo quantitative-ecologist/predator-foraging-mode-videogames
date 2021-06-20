22b3694626b438cc55ba85a2936a66e959d054cd
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2021-06-19 20:37:26 -0400

    added icc_tab3

--- R/03A_ICC.R
+++ R/03A_ICC.R
@@ -25,6 +25,8 @@ load("./outputs/03A_multivariate-model2.rda")
 mv_model2 <- mv_model
 rm(mv_model)
 
+load("./outputs/03A_multivariate-model3.rda")
+
 # =======================================================================
 # =======================================================================
 
@@ -37,9 +39,9 @@ rm(mv_model)
 # =======================================================================
 
 # Extract random effect standard deviations
-ran_var1 <- data.table(posterior_samples(mv_model1)[,c(5:16, 35:38)])
-ran_var2 <- data.table(posterior_samples(mv_model2)[,c(13:24, 43:46)])
-
+ran_var1 <- data.table(posterior_samples(mv_model1)[,c(13:24, 43:46)])
+ran_var2 <- data.table(posterior_samples(mv_model2)[,c(5:16, 35:38)])
+ran_var3 <- data.table(posterior_samples(mv_model3)[,c(17:28, 47:50)])
 
 # Compute variances for each random effect
 ran_var1[, c("speedvar_char", "spacevar_char", "guardvar_char", "hookvar_char",
@@ -56,6 +58,12 @@ ran_var2[, c("speedvar_char", "spacevar_char", "guardvar_char", "hookvar_char",
           lapply(.SD, function(x) {x^2}),
             .SDcols = c(1:16)][, c(1:16) := NULL]
 
+ran_var3[, c("speedvar_char", "spacevar_char", "guardvar_char", "hookvar_char",
+            "speedvar_map", "spacevar_map", "guardvar_map", "hookvar_map", 
+            "speedvar_id", "spacevar_id", "guardvar_id", "hookvar_id",
+            "speedvar_resid", "spacevar_resid", "guardvar_resid", "hookvar_resid") := 
+          lapply(.SD, function(x) {x^2}),
+            .SDcols = c(1:16)][, c(1:16) := NULL]
 
 # Compute total variance            
 ran_var1[, speedvar_total := rowSums(ran_var1[, c(1,5,9,13)])]
@@ -68,6 +76,10 @@ ran_var2[, spacevar_total := rowSums(ran_var2[, c(2,6,10,14)])]
 ran_var2[, guardvar_total := rowSums(ran_var2[, c(3,7,11,15)])]
 ran_var2[, hookvar_total := rowSums(ran_var2[, c(4,8,12,16)])]
 
+ran_var3[, speedvar_total := rowSums(ran_var3[, c(1,5,9,13)])]
+ran_var3[, spacevar_total := rowSums(ran_var3[, c(2,6,10,14)])]
+ran_var3[, guardvar_total := rowSums(ran_var3[, c(3,7,11,15)])]
+ran_var3[, hookvar_total := rowSums(ran_var3[, c(4,8,12,16)])]
 
 # Calculate ICCs
 # ID
@@ -81,6 +93,11 @@ ran_var2[, spaceicc_id := spacevar_id / spacevar_total]
 ran_var2[, guardicc_id := guardvar_id / guardvar_total]
 ran_var2[, hookicc_id := hookvar_id / hookvar_total]
 
+ran_var3[, speedicc_id := speedvar_id / speedvar_total]
+ran_var3[, spaceicc_id := spacevar_id / spacevar_total]
+ran_var3[, guardicc_id := guardvar_id / guardvar_total]
+ran_var3[, hookicc_id := hookvar_id / hookvar_total]
+
 # map
 ran_var1[, speedicc_map := speedvar_map / speedvar_total]
 ran_var1[, spaceicc_map := spacevar_map / spacevar_total]
@@ -91,6 +108,11 @@ ran_var2[, speedicc_map := speedvar_map / speedvar_total]
 ran_var2[, spaceicc_map := spacevar_map / spacevar_total]
 ran_var2[, guardicc_map := guardvar_map / guardvar_total]
 ran_var2[, hookicc_map := hookvar_map / hookvar_total]
+
+ran_var3[, speedicc_map := speedvar_map / speedvar_total]
+ran_var3[, spaceicc_map := spacevar_map / spacevar_total]
+ran_var3[, guardicc_map := guardvar_map / guardvar_total]
+ran_var3[, hookicc_map := hookvar_map / hookvar_total]
 
 # character
 ran_var1[, speedicc_char := speedvar_char / speedvar_total]
@@ -103,6 +125,11 @@ ran_var2[, spaceicc_char := spacevar_char / spacevar_total]
 ran_var2[, guardicc_char := guardvar_char / guardvar_total]
 ran_var2[, hookicc_char := hookvar_char / hookvar_total]
 
+ran_var3[, speedicc_char := speedvar_char / speedvar_total]
+ran_var3[, spaceicc_char := spacevar_char / spacevar_total]
+ran_var3[, guardicc_char := guardvar_char / guardvar_total]
+ran_var3[, hookicc_char := hookvar_char / hookvar_total]
+
 # residuals
 ran_var1[, speedicc_resid := speedvar_resid / speedvar_total]
 ran_var1[, spaceicc_resid := spacevar_resid / spacevar_total]
@@ -114,6 +141,10 @@ ran_var2[, spaceicc_resid := spacevar_resid / spacevar_total]
 ran_var2[, guardicc_resid := guardvar_resid / guardvar_total]
 ran_var2[, hookicc_resid := hookvar_resid / hookvar_total]
 
+ran_var3[, speedicc_resid := speedvar_resid / speedvar_total]
+ran_var3[, spaceicc_resid := spacevar_resid / spacevar_total]
+ran_var3[, guardicc_resid := guardvar_resid / guardvar_total]
+ran_var3[, hookicc_resid := hookvar_resid / hookvar_total]
 
 # Create table with mean icc and credibility interval
 lower_interval <- function (x) {coda::HPDinterval(as.mcmc(x), 0.95)[1]}
@@ -143,12 +174,29 @@ icc_tab2 <- data.table(group = c("speedicc_id", "spaceicc_id", "guardicc_id", "h
                                                    .SDcols = c(21:36)])
                         )
 
+icc_tab3 <- data.table(group = c("speedicc_id", "spaceicc_id", "guardicc_id", "hookicc_id",
+                                "speedicc_map", "spaceicc_map", "guardicc_map", "hookicc_map",
+                                "speedicc_char", "spaceicc_char", "guardicc_char", "hookicc_char",
+                                "speedicc_resid", "spaceicc_resid", "guardicc_resid", "hookicc_resid"),
+                       mean = as.numeric(ran_var3[, lapply(.SD, mean),
+                                                   .SDcols = c(21:36)]),
+                       lower = as.numeric(ran_var3[, lapply(.SD, lower_interval),
+                                                   .SDcols = c(21:36)]),
+                       upper = as.numeric(ran_var3[, lapply(.SD, upper_interval),
+                                                   .SDcols = c(21:36)])
+                        )
+
 icc_tab1[, ranef_variable := c(rep("id", 4),
                                rep("map", 4),
                                rep("character", 4),
                                rep("resid", 4))]
 
 icc_tab2[, ranef_variable := c(rep("id", 4),
+                               rep("map", 4),
+                               rep("character", 4),
+                               rep("resid", 4))]
+
+icc_tab3[, ranef_variable := c(rep("id", 4),
                                rep("map", 4),
                                rep("character", 4),
                                rep("resid", 4))]
@@ -167,6 +215,8 @@ icc_tab2[, ranef_variable := c(rep("id", 4),
 saveRDS(icc_tab1, file = "./outputs/03A_icc-table1.rds")
 
 saveRDS(icc_tab2, file = "./outputs/03A_icc-table2.rds")
+
+saveRDS(icc_tab3, file = "./outputs/03A_icc-table3.rds")
 
 # =======================================================================
 # =======================================================================
