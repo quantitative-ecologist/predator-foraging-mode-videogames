a0786711a579965d91a12978c39c22030b26bf4b
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2021-11-28 21:01:53 -0500

    .sh files for hunting success ready base models

--- jobs/03B_hunting_success_base-model1.sh
+++ jobs/03B_hunting_success_base-model1.sh
@@ -2,8 +2,8 @@
 #SBATCH --account=def-monti
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=40
-#SBATCH --mem=70000M
-#SBATCH --time=3-00:00
+#SBATCH --mem=64000M
+#SBATCH --time=1-12:00
 #SBATCH --mail-user=fraser_franco.maxime@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 
@@ -12,4 +12,4 @@ module load r/4.0.2
 module load r-bundle-bioconductor/3.12
 
 #export R_LIBS=~/.local/R/$EBVERSIONR/
-Rscript --no-save --no-restore hunting_success_base-model1.R
+Rscript --no-save --no-restore 03B_hunting_success_base-model1.R
