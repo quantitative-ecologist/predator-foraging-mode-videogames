8c70c6faa61e523503cd430cbfed26a9a98004a6
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2021-12-08 17:32:15 -0500

    first cross-validation files for base-model1

--- code/code_model-validation/03B_validation_base-model1.R
+++ code/code_model-validation/03B_validation_base-model1.R
@@ -10,57 +10,48 @@
 
 
 # =======================================================================
-# 1. Load the libraries, the dataset, and the model
+# 1. Load the libraries and the model
 # =======================================================================
 
 
 # Import libraries ------------------------------------------------------ 
 
-library(data.table)
 library(brms)
-library(ggplot2)
-library(ggpubr)
-
-
-
-# Import the data -------------------------------------------------------
-
-data <- fread("./data/merged-data2021.csv",
-              select = c("player_id", "match_id",
-                         "hunting_success", "map_name", 
-                         "game_duration", "speed",
-                         "space_covered_rate",
-                         "prox_mid_PreyGuarding",
-                         "hook_start_time"),
-                         stringsAsFactors = TRUE)
-
-
-
-# Transform --------------------------------------------------------------
-
-# Transform the data even though it is not perfect
-data[, ":=" (prox_mid_PreyGuarding = log(prox_mid_PreyGuarding + 1),
-             hook_start_time = log(hook_start_time + 1),
-             game_duration = sqrt(game_duration))]
-
-
-
-# Standardise the variables (Z-scores) ----------------------------------
-
-standardize <- function (x) {(x - mean(x, na.rm = TRUE)) / 
-                              sd(x, na.rm = TRUE)}
-
-data[, c("Zgame_duration", "Zspeed",
-         "Zspace_covered_rate", "Zprox_mid_PreyGuarding",
-         "Zhook_start_time") :=
-                lapply(.SD, standardize), 
-                .SDcols = c(5:9)]
-
-# Add observation-level random effect
-data$obs <- 1:nrow(data)
 
 
 
 # Load the model --------------------------------------------------------
 
 base_model <- readRDS("./outputs/models/03B_hunting_success_base-model1.rds")
+
+# =======================================================================
+# =======================================================================
+
+
+
+
+
+# =======================================================================
+# Perform the k-fold cross-validation
+# =======================================================================
+
+# Load the future package to parallelize the folds
+library(future)
+plan(multiprocess)
+
+# Function for the stratified K-fold cross-validation
+# We use player_id as the grouping factor that will be stratified.
+# This will ensure that the relative frequencies of each player
+# will stay approximately equivalent
+
+cv_object <- kfold(base_model,
+                   K = 10,
+                   Ksub = NULL,
+                   folds = "stratified",
+                   group = "player_id",
+                   chains = 1)
+
+saveRDS(cv_object, file = "kfoldcv-base_model1.rds")
+
+# =======================================================================
+# =======================================================================
