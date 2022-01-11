7a6d90639db03a710faca036bf48f9c448cf4ff6
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2022-01-11 14:53:13 -0500

    updated all job files (modules)

--- jobs/03B_conditional_effects.sh
+++ jobs/03B_conditional_effects.sh
@@ -9,8 +9,8 @@
 
 module load StdEnv/2020
 module load gcc/9.3.0
-module load r/4.0.2
-module load r-bundle-bioconductor/3.12
+module load r-bundle-bioconductor/3.14
+module load r/4.1.2
 
 #export R_LIBS=~/.local/R/$EBVERSIONR/
 Rscript --no-save --no-restore 03B_conditional_effects.R
--- jobs/03C_hunting_success_quadratic-model1.sh
+++ jobs/03C_hunting_success_quadratic-model1.sh
@@ -7,9 +7,10 @@
 #SBATCH --mail-user=fraser_franco.maxime@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 
-module load StdEnv/2020 gcc/9.3.0
-module load r/4.0.2
-module load r-bundle-bioconductor/3.12
+module load StdEnv/2020
+module load gcc/9.3.0
+module load r-bundle-bioconductor/3.14
+module load r/4.1.2
 
 #export R_LIBS=~/.local/R/$EBVERSIONR/
 Rscript --no-save --no-restore 03C_hunting_success_quadratic-model1.R
