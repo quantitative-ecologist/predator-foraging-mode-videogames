81782d8a6b894edcb6a3e086297aa20608b91d28
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2022-01-12 16:33:08 -0500

    mode comparison file

--- code/code_model-validation/model_comparison.R
+++ code/code_model-validation/model_comparison.R
@@ -38,5 +38,42 @@ loo4 <- readRDS("./outputs/R_objects/03c_loo_quadratic-model2.rds")
 # 2. Compare the models with the loo table
 # =======================================================================
 
-loo_compare(loo1, loo2)
-loo_compare(loo2, loo3)
+
+# Compute the table -----------------------------------------------------
+
+loo_table <- loo_compare(loo1, loo2, loo3, loo4)
+
+loo_table <- data.table(print(loo_table, simplify = FALSE),
+                        keep.rownames = TRUE)
+
+
+
+# Arrange the table -----------------------------------------------------
+
+# Change the first column name
+setnames(loo_table, "rn", "model")
+
+# Change the model names for proper names
+loo_table[1, 1 := "quadratic model2"]
+loo_table[2, 1 := "quadratic model1"]
+loo_table[3, 1 := "base model2"]
+loo_table[4, 1 := "base model1"]
+
+# Keep the columns of interest
+loo_table[, c(6:9) := NULL]
+
+# =======================================================================
+# =======================================================================
+
+
+
+
+
+# =======================================================================
+# 3. Export the LOO-CV table
+# =======================================================================
+
+saveRDS(loo_table, file = "./outputs/R_objects/loo-cv-table.rds")
+
+# =======================================================================
+# =======================================================================
